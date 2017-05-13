91b57a8aaeb664c752bde3c5eb2febc7c6f813d0
PennyNeko <penyamy@gmail.com>
2017-05-13 21:29:30 +0300

    added fitness evaluation

--- ColouringProblem/ColouringProblem/Source.cpp
+++ ColouringProblem/ColouringProblem/Source.cpp
@@ -4,7 +4,14 @@
 #include<list>
 #include<vector>
 #include <string>
+#include <random>
+#include "Source.h"
+
 std::vector< std::vector<int> > graph;
+int const GRAPH_BLOCKS = 16;
+int const POPULATION_SIZE = 100;
+int population[POPULATION_SIZE][GRAPH_BLOCKS];
+std::vector<int>fitness;
 
 std::vector< std::vector<int> > touchingBlocks() {
 	std::string line, number = "";
@@ -30,8 +37,6 @@ std::vector< std::vector<int> > touchingBlocks() {
 				//push the vector in the main vector and switch to the next line
 			}
 			graph.push_back(subgraph);
-			// TODO: remove; is unnecessary
-			subgraph.erase(subgraph.begin(), subgraph.end());
 		}
 		colouredBlocks.close();
 	}
@@ -40,26 +45,75 @@ std::vector< std::vector<int> > touchingBlocks() {
 	return graph;
 }
 
-void printGraph() {
+void const printGraph() {
 
 	//printing the vector
-	for (int i = 0; i < graph.size(); i++) {
-		for (int j = 0; j < graph[i].size(); j++) {
+	for (int i = 0; i < graph.size(); ++i) {
+		for (int j = 0; j < graph[i].size(); ++j) {
 			std::cout << graph[i][j] << " ";
 		}
 		std::cout << "\n";
 	}
 }
 
-void initialisePopulation() {
-
+ void initialisePopulation() {
+	 
+	std::random_device rd;  //Will be used to obtain a seed for the random number engine
+	std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+	std::uniform_int_distribution<> dis(0, 3);
+	for (int i = 0; i < POPULATION_SIZE; ++i) {
+		for (int j = 0; j < GRAPH_BLOCKS; ++j) {
+			population[i][j] = dis(gen);
+		}
+	}
 }
+
+ void const printPopulation() {
+	 for (int i = 0; i < POPULATION_SIZE; ++i) {
+		 for (int j = 0; j < GRAPH_BLOCKS; ++j) {
+			 std::cout << population[i][j] << " ";
+		 }
+		 std::cout << "\n";
+	 }
+ }
+
+std::vector<int> initializeFitness() {
+	 for (int i = 0; i < POPULATION_SIZE; ++i) {
+		 fitness.push_back(0);
+	 }
+	 return fitness;
+ }
+
+std::vector<int> applyFitness() {
+	 for (int i = 0; i < POPULATION_SIZE; ++i) {
+		 for (int j = 0; j < GRAPH_BLOCKS; ++j) {
+			 for (int k = 0; k < graph[j].size(); ++k) {
+				 if (population[i][j] == population[i][graph[j][k] - 1]) {
+					 fitness.at(i) += 0;
+				 }
+				 else {
+					 fitness.at(i) += 1;
+				 }
+			 }
+		 }
+	 }
+	 return fitness;
+ }
+
+ void const printFitness(std::vector<int> fitness) {
+	 for (int i = 0; i < POPULATION_SIZE; ++i) {
+		 std::cout << fitness[i] << "\n";
+	 }
+ }
 
 int main() {
 	graph = touchingBlocks();
-	int population[100][16];
-
-
-	std::cin >> i;
+	initialisePopulation();
+	printPopulation();
+	fitness = initializeFitness();
+	fitness = applyFitness();
+	printFitness(fitness);
+	int i;
+	std::cin >>  i;
 	return 0;
 }
